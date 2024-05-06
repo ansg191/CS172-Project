43f987388a0836e405e285c6168df7df5b3a7b09
Anshul Gupta <ansg191@anshulg.com>
2024-05-06 16:42:25 -0700

    Remove settings from `scraper.sh`

--- scraper.sh
+++ scraper.sh
@@ -3,11 +3,5 @@
 # Install the required packages
 python3 -m pip install -r requirements.txt
 
-# Set settings
-export SCRAPY_MAX_FILE_SIZE_GB="0.5"
-export SCRAPY_MAX_CONCURRENT_REQUESTS="8"
-export SCRAPY_MAX_REQUESTS_PER_DOMAIN="4"
-export SCRAPY_OUTPUT_FILE="computerscience_data.csv"
-
 # Run the scraper
 python3 scraper.py
