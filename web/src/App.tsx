1c2b5d870a90d5328e7aac362badf45beb7a87ed
Anshul Gupta <ansg191@anshulg.com>
2024-06-07 21:27:37 -0700

    Add group member names & improve styling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,14 @@ import Doc from "./Doc.tsx";
 import './App.css'
 import {isResultDoc, ResultDoc} from "./resultdoc.ts";
 
+const group_members = [
+    {name: "Anshul Gupta", sid: "862319580", nid: "agupt109"},
+    {name: "Nikhil Mahendrakar", sid: "862464249", nid: "nmahe008"},
+    {name: "Ishaan Bijor", sid: "862128714", nid: "ibijo001"},
+    {name: "Junbo Yang", sid: "862234040", nid: "jyang389"},
+    {name: "Junyan Hou", sid: "862394589", nid: "jhou038"},
+]
+
 function App() {
     const [query, setQuery] = useState('');
     const [data, setData] = useState<ResultDoc[]>([]);
@@ -29,6 +37,20 @@ function App() {
     return (
         <>
             <h1>CS172 Project</h1>
+            {data.length == 0 && query.length == 0 &&
+                <div>
+                    <h2 className="group-title">Group 10</h2>
+                    <table className="group-table">
+                        <tbody>
+                        {group_members.map(member => <tr>
+                            <td>{member.name}</td>
+                            <td><a href={`mailto:${member.nid}@ucr.edu`}>{member.nid}</a></td>
+                            <td>{member.sid}</td>
+                        </tr>)}
+                        </tbody>
+                    </table>
+                </div>
+            }
             <div className="card">
                 <input className="searchbox" type="text" value={query} onChange={(e) => setQuery(e.target.value)}/>
                 {/*Add this later if search becomes too long*/}
