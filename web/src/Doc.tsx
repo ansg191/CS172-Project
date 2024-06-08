cb1e56050c778661dd2ef45f02c86b023c5f6b14
Anshul Gupta <ansg191@anshulg.com>
2024-06-07 18:32:16 -0700

    Add image to search results

--- web/src/Doc.tsx
+++ web/src/Doc.tsx
@@ -9,14 +9,19 @@ function Doc({document}: DocParams) {
     return (
         <div className="card">
             <div className="card-body">
-                <h2 className='doc-title'>
-                    <a href={document.url} target="_blank" rel="noreferrer noopener">
-                        {document.title}
-                    </a>
-                </h2>
-                <div className='doc-domain-score'>
-                    <p className='doc-domain'>{document.domain}</p>
-                    <p className='doc-domain'>Score: {(document.score).toFixed(2)}</p>
+                <div className="image-container">
+                    <img className="image" src={document.images[0]} alt={document.title}/>
+                </div>
+                <div>
+                    <h2 className='doc-title'>
+                        <a href={document.url} target="_blank" rel="noreferrer noopener">
+                            {document.title}
+                        </a>
+                    </h2>
+                    <div className='doc-domain-score'>
+                        <p className='doc-domain'>{document.domain}</p>
+                        <p className='doc-domain'>Score: {(document.score).toFixed(2)}</p>
+                    </div>
                 </div>
             </div>
         </div>
